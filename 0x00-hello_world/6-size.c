6ab9beaca1468045619867a60d73fa31134a2a59
lungavncube <lungav.ncube@gmail.com>
2022-09-11 12:28:59 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,30 @@
+/*
+ * Size is not grandeur,
+ * and territory does not make a nation task
+ */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints the size of various types
+ * on the computer it is compiled and run on
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char acharater;
+	int aintiger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(acharacter);
+	printf("Size of an int: %zu byte(s)\n", sizeof(aintiger));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %zu byte(s)", sizeod(alonglong));
+	prinf("Size of a float: %zu byte(s)", sizeof(afloat));
+
+	return (0);
+}
