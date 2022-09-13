bac1db4695138f44d8ede6abac69d85afd2d6ff6
lungavncube <lungav.ncube@gmail.com>
2022-09-13 14:37:44 -0700

    print lowercase alphabet in reverse using putchar followed by new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
