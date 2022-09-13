08c9401af4ba1523142cf218801041437d76aa3a
lungavncube <lungav.ncube@gmail.com>
2022-09-13 03:44:59 -0700

    program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: program prints all single numbers
+ * of bas 10 starting from 0 followed by new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+return (0);
+}
