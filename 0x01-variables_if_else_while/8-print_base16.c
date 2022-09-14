8afebff2796d32abf2a21e080f6c1df124340316
lungavncube <lungav.ncube@gmail.com>
2022-09-14 02:16:01 -0700

    functions, nested loops directory readme file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all numbers of bas 16 in ;ower case,
+ * only using putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = '0'; c < '9'; c++)
+	{
+	putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+	 putchar(c);
+	}
+	putchar('\n');
+return: (0);
+}
