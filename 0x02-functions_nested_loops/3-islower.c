aafd04172d901d71f3671612baf7e26c49e3be7d
lungavncube <lungav.ncube@gmail.com>
2022-09-17 16:04:55 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - maint entry
+ *
+ * Description: function checks for lowercase character
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
