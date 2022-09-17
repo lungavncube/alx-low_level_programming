96fd8b9aa4f18e790f121ec3043713ab1d57ce20
lungavncube <lungav.ncube@gmail.com>
2022-09-17 14:22:49 -0700

    function prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: function prints the alphabet
+ * in lowercase
+ *
+ * Return: Always 0 success
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
