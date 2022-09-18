a925e90a85882ae24465a20929e3868753ad444f
lungavncube <lungav.ncube@gmail.com>
2022-09-17 17:21:31 -0700

    function checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ * @c: returns an intiger
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return(0);
+
+}
