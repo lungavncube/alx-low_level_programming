8c8b065ff987a8d260a939e775abf531d1c3d545
lungavncube <lungav.ncube@gmail.com>
2022-09-18 14:53:58 -0700

    adds two intigers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"                                                         
+
+                                                                          
+/**
+ * add - entry point
+ * @i: first intiger
+ * @j: second intiger
+ *
+ * Description: adds two intiger and returns results                      
+ *
+ * Return: sum of i and j
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
