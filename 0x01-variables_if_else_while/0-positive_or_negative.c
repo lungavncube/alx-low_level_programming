8fd06c2b9c94fff1ff9c39b3c60e231fa6969910
lungavncube <lungavncube@gmail.com>
2023-02-21 04:36:35 -0800

    program prints no. is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program will assign a random number
+ * to the variable n each time it is executed.
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0){
+		printf("%i is positive\n", n );
+	}
+	else if(n < 0){
+		printf("%i is negative\n", n);
+	}
+	else {
+		printf("%i is zero\n", n);
+	}
+	return (0);
+}
