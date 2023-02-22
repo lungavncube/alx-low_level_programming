723b548fd0e1875a6ae0580029b0256dcdeed5d6
lungavncube <lungavncube@gmail.com>
2023-02-22 11:02:48 -0800

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
