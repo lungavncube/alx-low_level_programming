ca21be9c4f492d6c4b2a7d8f785c6845c5956928
lungavncube <lungavncube@gmail.com>
2023-02-22 10:20:36 -0800

    print alpahbet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print lowercase alphabet in reverse
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
