b80e631e131c7c1fcb61ef653f42b5469d4f16b8
lungavncube <lungavncube@gmail.com>
2023-02-22 10:05:55 -0800

    print numbers using 'putchar'

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program prints all single numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
